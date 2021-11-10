3f55a4930738b81d4d76c1ec96b1ad28a7f05660
Dletta <jcduschletta@gmail.com>
2021-11-10 15:06:53 -0600

    debugging

--- plugins/splunk-field-value/index.js
+++ plugins/splunk-field-value/index.js
@@ -10,12 +10,13 @@ module.exports = class extends PluginLoaderBase {
       * @returns {string}
       */
       stringify: (token) => {
-        console.log(`Query: ${token.Children('log_stream_selector')[0].value} | json | ${token.Children('label')[1].value}=~${token.Children('quoted_str')[1].value}`)
         console.log(`Label array length ${token.Children('label').length}`)
         console.log(`Label array ${token.Children('label')}`)
         if (token.Children('label').length >= 1) {
+          console.log(`Query: ${token.Children('log_stream_selector')[0].value} | json | ${token.Children('label')[1].value}=~${token.Children('quoted_str')[1].value}`)
           return `${token.Children('log_stream_selector')[0].value} | json | ${token.Children('label')[1].value}=~${token.Children('quoted_str')[1].value}`
         } else if (token.Children('label').length > 2) {
+          console.log(`Query: ${token.Children('log_stream_selector')[0].value} | json | ${token.Children('label')[1].value}=~${token.Children('quoted_str')[1].value} or ${token.Children('label')[2].value}=~${token.Children('quoted_str')[2].value}`)
           return `${token.Children('log_stream_selector')[0].value} | json | ${token.Children('label')[1].value}=~${token.Children('quoted_str')[1].value} or ${token.Children('label')[2].value}=~${token.Children('quoted_str')[2].value} `
         }
       }
